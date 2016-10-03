9187e324930a09439ef377f494678abd936ca5a8
Carlos Alejandro Hernande Montalvo <parale23@hotmail.com>
2016-10-03 11:13:37 -0500

    modificar codigo

--- verificar.py
+++ verificar.py
@@ -3,19 +3,20 @@
 import unittest
 from verificaEdad import verificar
 
+
 def verificaEdad(edad):
- if edad <= 0:
- 	print "No existes"
- elif edad < 13 and edad >0:
- 	print "Eres niño"
- if edad < 18:
- 	print "Eres adolescente"	
- if edad < 65:
- 	print "Eres adulto"
- if edad < 120:
- 	print "Eres adulto mayor"
- else:
- 	print "Eres Mumm-Ra"
+    if edad <= 0:
+        print "No existes"
+    elif edad < 13 and edad > 0:
+        print "Eres niño"
+    if edad < 18:
+        print "Eres adolescente"
+    if edad < 65:
+        print "Eres adulto"
+    if edad < 120:
+        print "Eres adulto mayor"
+    else:
+        print "Eres Mumm-Ra"
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
